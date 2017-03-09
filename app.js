dc5a1d5a074b8994dce02b4f85cfc461514f923e
J. G. Manzano <unico.javier@gmail.com>
2017-03-09 19:58:24 +0100

    Trying Express routing

--- app.js
+++ app.js
@@ -14,9 +14,20 @@ const app = express();
 app.get('/test', (req, res, next) => res.send('GET request on /test'));
 
 /**
- * Crete POST handler
+ * Create POST handler
  */
 app.post('/ping', (req, res) => res.send('Post request on /ping'));
+
+app.route('/book')
+  .get(function (req, res) {
+    res.send('Get a random book')
+  })
+  .post(function (req, res) {
+    res.send('Add a book')
+  })
+  .put(function (req, res) {
+    res.send('Update the book')
+  });
 
 /**
  *  Start server

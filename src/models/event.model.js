9f29dea90cd72d36b6e15ce41c7a6bdd168bd2cd
J. G. Manzano <unico.javier@gmail.com>
2017-03-16 21:08:35 +0100

    added index in event.title

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -1,18 +1,10 @@
 'use strict';
 
-/**
- * Event model por testing API
- */
-// const events = [
-//     {id: 12345, title: 'Un evento', descripcion: 'asñkdlfhasdkfjh', fecha: '12/01/1998'},
-//     {id: 98663, title: 'Otro evento', descripcion: 'qwerty', fecha: '01/02/2001'}
-// ];
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-  title:  String,
+  title:  { type: String, index: true },
   description: String,
   date: { type: String },
 });

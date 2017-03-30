74d110d4dd528280115f947d16a195618d33ea2b
J. G. Manzano <unico.javier@gmail.com>
2017-03-31 00:39:22 +0200

    FIX - event endpoint tests

--- src/tests/event.endpoint.test.js
+++ src/tests/event.endpoint.test.js
@@ -30,11 +30,7 @@ const Event = mongoose.model('Event', eventSchema);
 
 describe('Event API endpoint', () => {
 
-    // Empty DB.
-    Event.collection.drop();
-
-    let id = '';
-
+    // Insert an event record before the tests.
     before( () => {
         let newEvent = new Event({
           title: 'test event',
@@ -42,18 +38,71 @@ describe('Event API endpoint', () => {
           date: '01/01/1990',
         });
 
-        console.log('Before saving');
         newEvent.save()
             .then(result =>{
                 id = result._id;
             })
             .catch(error => {
-
+                console.log(error);
             });
     });
 
-    after( () => {
-        //TODO: borrado de los datos de test en la BD.
+    // Empty the DB event table once tests are over.
+    after(function(done){
+      Event.collection.drop();
+      done();
+    });
+
+    describe('POST event', () => {
+        it('POST event receives an 201 status', (done) => {
+            chai.request(server)
+                .post('/event')
+                .send({id: 987654321, title: 'ola k ase', descripcion: 'pir pir pir', fecha: '12/01/3078'})
+                .end((err, res) => {
+                    expect(res).to.have.status(201);
+                    done();
+            });
+        });
+
+        it('POST event return value is the same as the sent', (done) => {
+            const sentData = {id: 987654321, title: 'ola k ase', descripcion: 'pir pir pir', fecha: '12/01/3078'};
+            chai.request(server)
+                .post('/event')
+                .send(sentData)
+                .end((err, res) => {
+                    expect(res.body.data).to.deep.equals(sentData);
+                    done();
+            });
+        });
+
+    });
+
+    describe('PUT event by Id', () => {
+        it('PUT event receives an 201 status', (done) => {
+            chai.request(server)
+                .put('/event/'+id)
+                .send({id: 987654321, title: 'ola k ase 2', descripcion: '', fecha: '01/11/1234'})
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    done();
+            });
+        });
+
+        it('PUT event updates the object and its not equals', (done) => {
+            const sentData = {id: 12345, title: 'ola k ase 2', descripcion: '', fecha: '01/11/1234'};
+            chai.request(server)
+            .post('/event')
+            .send(sentData)
+            .end((err, res) => {
+                chai.request(server)
+                    .put('/event/'+id)
+                    .send({id: 12345, title: 'titulo cambiado', descripcion: '', fecha: '01/11/1234'})
+                    .end((err, res) => {
+                        expect(res.body.data).to.not.deep.equals(sentData);
+                        done();
+                });
+            });
+        });
     });
 
     describe('GET all events', () => {
@@ -107,58 +156,6 @@ describe('Event API endpoint', () => {
             });
         });
 
-    });
-
-    describe('POST event', () => {
-        it('POST event receives an 201 status', (done) => {
-            chai.request(server)
-                .post('/event')
-                .send({id: 987654321, title: 'ola k ase', descripcion: 'pir pir pir', fecha: '12/01/3078'})
-                .end((err, res) => {
-                    expect(res).to.have.status(201);
-                    done();
-            });
-        });
-
-        it('POST event return value is the same as the sent', (done) => {
-            const sentData = {id: 987654321, title: 'ola k ase', descripcion: 'pir pir pir', fecha: '12/01/3078'};
-            chai.request(server)
-                .post('/event')
-                .send(sentData)
-                .end((err, res) => {
-                    expect(res.body.data).to.deep.equals(sentData);
-                    done();
-            });
-        });
-
-    });
-
-    describe('PUT event by Id', () => {
-        it('PUT event receives an 201 status', (done) => {
-            chai.request(server)
-                .put('/event/'+id)
-                .send({id: 987654321, title: 'ola k ase 2', descripcion: '', fecha: '01/11/1234'})
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    done();
-            });
-        });
-
-        it('PUT event updates the object and its not equals', (done) => {
-            const sentData = {id: 12345, title: 'ola k ase 2', descripcion: '', fecha: '01/11/1234'};
-            chai.request(server)
-            .post('/event')
-            .send(sentData)
-            .end((err, res) => {
-                chai.request(server)
-                    .put('/event/'+id)
-                    .send({id: 12345, title: 'titulo cambiado', descripcion: '', fecha: '01/11/1234'})
-                    .end((err, res) => {
-                        expect(res.body.data).to.not.deep.equals(sentData);
-                        done();
-                });
-            });
-        });
     });
 
     describe('DELETE event by Id', () => {
